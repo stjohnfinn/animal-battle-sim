75d09155b172fd6e72f08676ea01657e1470e230
stjohnfinn <stjohn.finn@gmail.com>
2022-01-31 21:53:27 -0800

    initial commit added everything

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    fontFamily: {
+      'title': 'Manrope',
+      'body': 'Open Sans'
+    },
+    colors: {
+      gray: '#363537',
+      blue: '#A1B5D8',
+      white: '#FFFCF7',
+      lightgray: 'gray',
+      transparent: 'transparent',
+      pink: 'pink'
+    }
+  },
+  plugins: [],
+}
